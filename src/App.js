bbe091701ed306fcbfeb89dde7f88bb8206daf19
keisuke_ito <cchord0402@yahoo.co.jp>
2021-06-27 12:11:57 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
 
@@ -16,13 +16,13 @@ function App() {
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    setNotes(apiData.data.listNotes.items);
+    const apiData = await API.graphql({ query: listTodos });
+    setNotes(apiData.data.listTodos.items);
   }
 
   async function createNote() {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
@@ -30,7 +30,7 @@ function App() {
   async function deleteNote({ id }) {
    const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
   }
 
   return (
